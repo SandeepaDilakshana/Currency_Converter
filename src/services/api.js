efa1992c03590798b432e09124f1349e90246fb6
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2024-12-24 16:29:57 +0530

    update api.js file

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
-const API_URL = 'https://api.exchangerate.host/latest';
+import { API_KEY } from '@env';
+
+const API_URL = `https://api.exchangerate.host/latest?apikey=${API_KEY}`;
+
 
 export const getExchangeRate = async (base, target) => {
   try {
-    const response = await axios.get(`${API_URL}?base=${base}&symbols=${target}`);
+    const response = await axios.get(`${API_URL}&base=${base}&symbols=${target}`);
     return response.data.rates[target];
   } catch (error) {
     console.error('Error fetching exchange rate:', error);
