f560480214ba83ad67d33bf53a09d88c77cdbeda
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2024-12-24 13:49:10 +0530

    feat: import PaperProvider SafeAreaView and KeyBoardAvoidingView

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
+import { PaperProvider, Text } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text variant="displayLarge">Currency Converter</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <SafeAreaView>
+        <KeyBoardAvoidingView>
+          <View style={styles.container}>
+            <Text variant="displayMedium">Currency Converter</Text>
+            <StatusBar style="auto" />
+          </View>
+        </KeyBoardAvoidingView>
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
