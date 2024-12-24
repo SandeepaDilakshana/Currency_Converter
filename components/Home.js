d3a163fadd113ecb86d9329228df6c5e3bbfec66
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2024-12-24 16:31:13 +0530

    fix:

--- components/Home.js
+++ components/Home.js
@@ -1,23 +1,23 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { TextInput, Button } from 'react-native-paper';
 import { getExchangeRate } from '../src/services/api';
 
-const handleConvert = async()=>{
-    const rate = await getExchangeRate(baseCurrency,targetCurrency)
-    if(rate){
+const handleConvert = async () => {
+    const rate = await getExchangeRate(baseCurrency, targetCurrency)
+    if (rate) {
         setResult(rate * parseFloat(amount));
     }
 }
 
-const [baseCurrency, setBaseCurrency] = useState('USD');
-const [targetCurrency, setTargetCurrency] = useState('EUR');
-const [amount,setAmount] = useState('');
-const [result,setResult] = useState(null);
 export default function Home() {
+    const [baseCurrency, setBaseCurrency] = useState('USD');
+    const [targetCurrency, setTargetCurrency] = useState('EUR');
+    const [amount, setAmount] = useState('');
+    const [result, setResult] = useState(null);
+
     return (
         <View>
-            <Text>Base Curency :</Text>
             <TextInput
                 label="Base Currency"
                 mode="outlined"
@@ -25,7 +25,6 @@ export default function Home() {
                 onChangeText={setBaseCurrency}
                 style={styles.input} />
 
-            <Text>Target Currency :</Text>
             <TextInput
                 label="Target Currency"
                 mode="outlined"
@@ -33,13 +32,17 @@ export default function Home() {
                 onChangeText={setTargetCurrency}
                 style={styles.input} />
 
-            <Text>Amount :</Text>
             <TextInput
                 label="Amount"
                 mode="outlined"
                 value={amount}
                 onChangeText={setAmount}
-                style={styles.input} />    
+                style={styles.input} />
+
+            <Button mode="outlined" onPress={handleConvert}>
+                Convert
+            </Button>
+            {result && <Text>Converted Amount: {result.toFixed(2)}</Text>}
         </View>
     )
 }
@@ -49,7 +52,6 @@ const styles = StyleSheet.create({
         padding: 20
     },
     input: {
-        borderWidth: 1,
         padding: 8,
         marginVertical: 10
     },
