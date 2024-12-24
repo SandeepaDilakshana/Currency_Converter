ab7699fbf81b9a3563bde6066f54f3e26690e885
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2024-12-24 15:28:01 +0530

    feat: add the basic TextInputs to Home.js

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,47 @@
+import { useState } from 'react';
 import { View, Text } from 'react-native'
+import { TextInput } from 'react-native-paper'
 
-
+const [baseCurrency, setBaseCurrency] = useState('USD');
+const [targetCurrency, setTargetCurrency] = useState('EUR');
+const [amount,setAmount] = useState('');
 export default function Home() {
-  return (
-    <View>
-      
-    </View>
-  )
+    return (
+        <View>
+            <Text>Base Curency :</Text>
+            <TextInput
+                label="Base Currency"
+                mode="outlined"
+                value={baseCurrency}
+                onChangeText={setBaseCurrency}
+                style={styles.input} />
+
+            <Text>Target Currency :</Text>
+            <TextInput
+                label="Target Currency"
+                mode="outlined"
+                value={targetCurrency}
+                onChangeText={setTargetCurrency}
+                style={styles.input} />
+
+            <Text>Amount :</Text>
+            <TextInput
+                label="Amount"
+                mode="outlined"
+                value={amount}
+                onChangeText={setAmount}
+                style={styles.input} />    
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20
+    },
+    input: {
+        borderWidth: 1,
+        padding: 8,
+        marginVertical: 10
+    },
+});
